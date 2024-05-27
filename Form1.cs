14e2e3ac1df62d69780e4ddad7d194af65b388ff
TjanKazar <tjan.kazar3@gmail.com>
2024-05-27 20:13:01 +0200

    added admin login only

--- Form1.cs
+++ Form1.cs
@@ -1,7 +1,5 @@
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -9,24 +7,41 @@ namespace ozra3_desktop
 {
 	public partial class Form1 : Form
 	{
-		HttpClient client = new HttpClient();
-		public string username;
-		public string password;
+		private HttpClient client = new HttpClient();
+
 		public Form1()
 		{
 			InitializeComponent();
 			client.BaseAddress = new Uri("http://127.0.0.1:5000");
 		}
 
-		
-
-		
-		private void Login_Click(object sender, EventArgs e)
+		private async void Login_Click(object sender, EventArgs e)
 		{
-			Form MainPage = new MainPage(client);
-			MainPage.Show();
+			string username = textBox1.Text;
+			string password = textBox2.Text;
+
+			HttpResponseMessage response = await client.GetAsync($"/uporabnik/{username}/{password}");
+
+			if (response.IsSuccessStatusCode)
+			{
+				string responseBody = await response.Content.ReadAsStringAsync();
+				dynamic userData = Newtonsoft.Json.JsonConvert.DeserializeObject(responseBody);
+
+
+				if (userData.accessible == true)
+				{
+					Form MainPage = new MainPage(client);
+					MainPage.Show();
+				}
+				else
+				{
+					MessageBox.Show("You do not have access to this feature", "Access Denied", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+				}
+			}
+			else
+			{
+				MessageBox.Show("User not found", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
 		}
-
-
 	}
 }
